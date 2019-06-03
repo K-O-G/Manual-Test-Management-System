834cbe11a0c8649b5f5153cb7ce3aee390e983c2
Katerina Gordiyenko <KaterinaO.Gordiyenko@livenau.net>
2019-06-03 11:07:45 +0300

    rollback script

--- WebUI/Scripts/main.js
+++ WebUI/Scripts/main.js
@@ -3,7 +3,7 @@
         var id = $(this).data("id");
         var selectListItem = $(this).parent().parent().find("select");
         var selectedItemText = selectListItem.find(':selected').text();
-        var selectedItemColor = selectListItem.find(':selected').color();
+//        var selectedItemColor = selectListItem.find(':selected').color();
         var resultId = selectListItem.val();
         $.ajax({
             url: '/CheckLists/SaveExecute',
@@ -16,7 +16,7 @@
                 var parentTD = selectListItem.parent();
                 selectListItem.remove();
                 parentTD[0].innerHTML = '<p>' + selectedItemText + '</p>';
-                parent[0].innerHTML = '<span style="color:' + selectedItemColor + '">' + selectedItemText + '</span>';
+//                parent[0].innerHTML = '<span style="color:' + selectedItemColor + '">' + selectedItemText + '</span>';
             }
         });
     });
